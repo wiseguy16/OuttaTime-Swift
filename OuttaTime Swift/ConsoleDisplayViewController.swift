b808c924e2b3cde3f5936429da06a8ad68fd1559
wiseguy16 <gergusa04@gmail.com>
2016-08-15 12:22:42 -0400

    initial storyboard

---
+++ OuttaTime Swift/ConsoleDisplayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConsoleDisplayViewController.swift
+//  OuttaTime Swift
+//
+//  Created by Gregory Weiss on 8/15/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ConsoleDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
