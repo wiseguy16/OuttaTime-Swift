6074569dcf913a3228842600f07e142938aa7db1
wiseguy16 <gergusa04@gmail.com>
2016-08-15 16:34:32 -0400

    First working version

--- OuttaTime Swift/ConsoleDisplayViewController.swift
+++ OuttaTime Swift/ConsoleDisplayViewController.swift
@@ -8,28 +8,106 @@
 
 import UIKit
 
-class ConsoleDisplayViewController: UIViewController {
+protocol SetDestinationViewControllerDelegate
+{
+    func didFinishTask(dateToSet: NSDate)
+}
 
-    override func viewDidLoad() {
+class ConsoleDisplayViewController: UIViewController, SetDestinationViewControllerDelegate
+{
+
+    
+    
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var presentDateLabel: UILabel!
+    @IBOutlet weak var lastDateLabel: UILabel!    
+    @IBOutlet weak var speedMPHLabel: UILabel!
+    let dateFormatter = NSDateFormatter()
+    var speedMPHtimer: NSTimer?
+    var currentSpeed: Int = 0
+    var originalSpeed: Int = 0
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        dateFormatter.dateStyle = .MediumStyle
+        dateFormatter.timeStyle = .NoStyle
+        let currentDate = NSDate()
+        presentDateLabel.text = dateFormatter.stringFromDate(currentDate)
+        destinationLabel.text = "Pick a Date!"
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+// ACTION HANDLERS
+    
+    
+    @IBAction func travelBackTapped(sender: UIButton)
+    {
+       // speedMPHtimer.invalidate()
+        
+        speedMPHtimer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "travelBackFired", userInfo: nil, repeats: true)
+        
+       // print("line 56")
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if segue.identifier == "SetDestinationSegue"
+        {
+            let setDateVC = segue.destinationViewController as! SetDestinationViewController
+            setDateVC.delegate = self
+            
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        }
     }
-    */
+    
+    func didFinishTask(dateToSet: NSDate)
+    {
+       destinationLabel.text = dateFormatter.stringFromDate(dateToSet)
+        
+    }
+    
+    func travelBackFired()
+    {
+        //print("line 85")
+        // Going to count up to 88!!!
+        if (originalSpeed < 88)
+        {
+            originalSpeed = originalSpeed + 1
+            speedMPHLabel.text = "\(originalSpeed) MPH"
+        }
+        else
+        {
+          speedMPHtimer?.invalidate()
+            originalSpeed = 0
+            speedMPHLabel.text = "0 MPH"
+            lastDateLabel.text = presentDateLabel.text
+            presentDateLabel.text = destinationLabel.text
+            destinationLabel.text = "Pick a date!"
+        }
+    }
+
+    
 
 }
+
+//extension ConsoleDisplayViewController: DetailViewControllerDelegate {
+//    func didFinishTask(Sender: SetDestinationViewController){
+//        
+//        }
+//}
--- OuttaTime Swift/SetDestinationViewController.swift
+++ OuttaTime Swift/SetDestinationViewController.swift
@@ -9,19 +9,41 @@
 import UIKit
 
 class SetDestinationViewController: UIViewController {
+    
+     var delegate:SetDestinationViewControllerDelegate?
 
-    override func viewDidLoad() {
+    @IBOutlet weak var setDatePicker: UIDatePicker!
+    
+    let dateFormatter = NSDateFormatter()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
+    
+    @IBAction func setTapped(sender: UIButton)
+    {
+        
+        
+        delegate!.didFinishTask(setDatePicker.date)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func cancelTapped(sender: UIButton)
+    {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
